3b82b9d9b0cdaa2e185a9916d8ad83fd5ad49fea
deakjanos <deaak.jaanos@gmail.com>
2013-02-03 15:13:57 +0100

    Telkek

---
+++ telek/telek/telek/Form1.Designer.vb
@@ -0,0 +1,363 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.beolvas = New System.Windows.Forms.Button
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.beolvas2 = New System.Windows.Forms.Button
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.tombbeolvas = New System.Windows.Forms.Button
+        Me.tombbeolvasott = New System.Windows.Forms.TextBox
+        Me.TextBox3 = New System.Windows.Forms.TextBox
+        Me.TextBox4 = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.joletsortext = New System.Windows.Forms.TextBox
+        Me.gazdagsortext = New System.Windows.Forms.TextBox
+        Me.TextBox6 = New System.Windows.Forms.TextBox
+        Me.TextBox7 = New System.Windows.Forms.TextBox
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.joletsorhsztext = New System.Windows.Forms.TextBox
+        Me.Label7 = New System.Windows.Forms.Label
+        Me.gazdagsorhsztext = New System.Windows.Forms.TextBox
+        Me.joletsoreredmenyek = New System.Windows.Forms.TextBox
+        Me.Label8 = New System.Windows.Forms.Label
+        Me.gazdagsoreredmenyek = New System.Windows.Forms.TextBox
+        Me.Label9 = New System.Windows.Forms.Label
+        Me.Label10 = New System.Windows.Forms.Label
+        Me.gazdagsorteruletszerinttext = New System.Windows.Forms.TextBox
+        Me.SuspendLayout()
+        '
+        'beolvas
+        '
+        Me.beolvas.Location = New System.Drawing.Point(13, 11)
+        Me.beolvas.Name = "beolvas"
+        Me.beolvas.Size = New System.Drawing.Size(153, 43)
+        Me.beolvas.TabIndex = 0
+        Me.beolvas.Text = "Beolvasás fájlból soronként"
+        Me.beolvas.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(173, 13)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(226, 41)
+        Me.TextBox1.TabIndex = 1
+        '
+        'beolvas2
+        '
+        Me.beolvas2.Location = New System.Drawing.Point(406, 13)
+        Me.beolvas2.Name = "beolvas2"
+        Me.beolvas2.Size = New System.Drawing.Size(140, 41)
+        Me.beolvas2.TabIndex = 2
+        Me.beolvas2.Text = "Szeparátorok használata"
+        Me.beolvas2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(553, 13)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox2.Size = New System.Drawing.Size(196, 41)
+        Me.TextBox2.TabIndex = 3
+        '
+        'tombbeolvas
+        '
+        Me.tombbeolvas.Location = New System.Drawing.Point(13, 61)
+        Me.tombbeolvas.Name = "tombbeolvas"
+        Me.tombbeolvas.Size = New System.Drawing.Size(153, 44)
+        Me.tombbeolvas.TabIndex = 4
+        Me.tombbeolvas.Text = "Beolvasás tömbbe"
+        Me.tombbeolvas.UseVisualStyleBackColor = True
+        '
+        'tombbeolvasott
+        '
+        Me.tombbeolvasott.Location = New System.Drawing.Point(173, 82)
+        Me.tombbeolvasott.Multiline = True
+        Me.tombbeolvasott.Name = "tombbeolvasott"
+        Me.tombbeolvasott.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tombbeolvasott.Size = New System.Drawing.Size(226, 44)
+        Me.tombbeolvasott.TabIndex = 5
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(405, 82)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox3.Size = New System.Drawing.Size(343, 44)
+        Me.TextBox3.TabIndex = 6
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(12, 160)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox4.Size = New System.Drawing.Size(62, 290)
+        Me.TextBox4.TabIndex = 7
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(172, 61)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(185, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Soronkénti beolvasás (u.a., mint fent):"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(403, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(322, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Egyetlen sztring az összes beolvasott adatból (Join, konkatenáció):"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 144)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(140, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "A teljes sztring leválogatása:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(77, 163)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(82, 13)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Jólétsor adatok:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(251, 163)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(97, 13)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Gazdagsor adatok:"
+        '
+        'joletsortext
+        '
+        Me.joletsortext.Location = New System.Drawing.Point(80, 179)
+        Me.joletsortext.Multiline = True
+        Me.joletsortext.Name = "joletsortext"
+        Me.joletsortext.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.joletsortext.Size = New System.Drawing.Size(150, 53)
+        Me.joletsortext.TabIndex = 15
+        '
+        'gazdagsortext
+        '
+        Me.gazdagsortext.Location = New System.Drawing.Point(254, 179)
+        Me.gazdagsortext.Multiline = True
+        Me.gazdagsortext.Name = "gazdagsortext"
+        Me.gazdagsortext.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.gazdagsortext.Size = New System.Drawing.Size(145, 53)
+        Me.gazdagsortext.TabIndex = 16
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(661, 160)
+        Me.TextBox6.Multiline = True
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox6.Size = New System.Drawing.Size(55, 290)
+        Me.TextBox6.TabIndex = 17
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(722, 160)
+        Me.TextBox7.Multiline = True
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox7.Size = New System.Drawing.Size(50, 290)
+        Me.TextBox7.TabIndex = 18
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(80, 235)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(123, 13)
+        Me.Label4.TabIndex = 19
+        Me.Label4.Text = "Jólétsor házszám szerint:"
+        '
+        'joletsorhsztext
+        '
+        Me.joletsorhsztext.Location = New System.Drawing.Point(80, 251)
+        Me.joletsorhsztext.Multiline = True
+        Me.joletsorhsztext.Name = "joletsorhsztext"
+        Me.joletsorhsztext.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.joletsorhsztext.Size = New System.Drawing.Size(149, 64)
+        Me.joletsorhsztext.TabIndex = 20
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(254, 235)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(138, 13)
+        Me.Label7.TabIndex = 21
+        Me.Label7.Text = "Gazdagsor házszám szerint:"
+        '
+        'gazdagsorhsztext
+        '
+        Me.gazdagsorhsztext.Location = New System.Drawing.Point(254, 251)
+        Me.gazdagsorhsztext.Multiline = True
+        Me.gazdagsorhsztext.Name = "gazdagsorhsztext"
+        Me.gazdagsorhsztext.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.gazdagsorhsztext.Size = New System.Drawing.Size(142, 64)
+        Me.gazdagsorhsztext.TabIndex = 22
+        '
+        'joletsoreredmenyek
+        '
+        Me.joletsoreredmenyek.Location = New System.Drawing.Point(80, 348)
+        Me.joletsoreredmenyek.Multiline = True
+        Me.joletsoreredmenyek.Name = "joletsoreredmenyek"
+        Me.joletsoreredmenyek.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.joletsoreredmenyek.Size = New System.Drawing.Size(149, 102)
+        Me.joletsoreredmenyek.TabIndex = 23
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(83, 329)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(167, 13)
+        Me.Label8.TabIndex = 24
+        Me.Label8.Text = "Jólétsor: hsz, szél, hossz, ter., adó"
+        '
+        'gazdagsoreredmenyek
+        '
+        Me.gazdagsoreredmenyek.Location = New System.Drawing.Point(254, 348)
+        Me.gazdagsoreredmenyek.Multiline = True
+        Me.gazdagsoreredmenyek.Name = "gazdagsoreredmenyek"
+        Me.gazdagsoreredmenyek.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.gazdagsoreredmenyek.Size = New System.Drawing.Size(138, 102)
+        Me.gazdagsoreredmenyek.TabIndex = 25
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(257, 329)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(179, 13)
+        Me.Label9.TabIndex = 26
+        Me.Label9.Text = "Gazdagsor:hsz, szél, hossz, ter., adó"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(438, 162)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(132, 13)
+        Me.Label10.TabIndex = 27
+        Me.Label10.Text = "Gazdagasor terület szerint:"
+        '
+        'gazdagsorteruletszerinttext
+        '
+        Me.gazdagsorteruletszerinttext.Location = New System.Drawing.Point(441, 179)
+        Me.gazdagsorteruletszerinttext.Multiline = True
+        Me.gazdagsorteruletszerinttext.Name = "gazdagsorteruletszerinttext"
+        Me.gazdagsorteruletszerinttext.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.gazdagsorteruletszerinttext.Size = New System.Drawing.Size(174, 136)
+        Me.gazdagsorteruletszerinttext.TabIndex = 28
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 462)
+        Me.Controls.Add(Me.gazdagsorteruletszerinttext)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.gazdagsoreredmenyek)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.joletsoreredmenyek)
+        Me.Controls.Add(Me.gazdagsorhsztext)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.joletsorhsztext)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.gazdagsortext)
+        Me.Controls.Add(Me.joletsortext)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.tombbeolvasott)
+        Me.Controls.Add(Me.tombbeolvas)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.beolvas2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.beolvas)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents beolvas As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents beolvas2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents tombbeolvas As System.Windows.Forms.Button
+    Friend WithEvents tombbeolvasott As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents joletsortext As System.Windows.Forms.TextBox
+    Friend WithEvents gazdagsortext As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents joletsorhsztext As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents gazdagsorhsztext As System.Windows.Forms.TextBox
+    Friend WithEvents joletsoreredmenyek As System.Windows.Forms.TextBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents gazdagsoreredmenyek As System.Windows.Forms.TextBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents gazdagsorteruletszerinttext As System.Windows.Forms.TextBox
+
+End Class
---
+++ telek/telek/telek/Form1.vb
@@ -0,0 +1,273 @@
+﻿Public Class Form1
+
+    Private Sub beolvas_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles beolvas.Click
+        Dim f As New IO.StreamReader("telkek.txt")
+        Dim Telekszam As Long
+
+        Telekszam = CInt(f.ReadLine)
+        'Dim telek()
+        Dim word(Telekszam - 1)
+        Dim telek(Telekszam - 1) As String
+
+        For i As Integer = 0 To Telekszam - 1
+            Dim sor As String = f.ReadLine
+            telek(i) = sor
+
+            TextBox1.Text = TextBox1.Text + telek(i) + Chr(13) + Chr(10)
+
+        Next
+
+    End Sub
+
+    Private Sub beolvas2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles beolvas2.Click
+        
+
+        
+        ' Néhány string szóközzel, vesszővel és adott sztringgel "KKK" elválasztva
+
+        Dim x As Integer
+
+        Dim intSpacedString As String = "1 3 5 7 9"
+        Dim charCommaString As String = " a, e, i, o, u"
+        Dim nameCommaString As String = "Sándor,József,,,Benedek,Ádám,Éva,Joli,Béla"
+        Dim subStringString As String = "12KKK34KKK56KKK78KKK"
+        Dim spaceSeparator As Char() = New Char() {" "c}
+        Dim commaSeparator As Char() = New Char() {","c}
+        Dim stringSeparators As String() = New String() {"KKK"}
+        Dim result As String()
+
+
+        result = intSpacedString.Split(spaceSeparator, StringSplitOptions.None)
+
+        For Each str As String In result
+            x = CInt(str)
+            str = CStr(x)
+            TextBox2.Text = TextBox2.Text + str + Chr(13) + Chr(10)
+        Next
+        
+        result = charCommaString.Split(commaSeparator, StringSplitOptions.None)
+
+        For Each str As String In result
+            TextBox2.Text = TextBox2.Text + str + Chr(13) + Chr(10)
+        Next
+
+        result = nameCommaString.Split(commaSeparator, StringSplitOptions.None)
+
+        For Each str As String In result
+            TextBox2.Text = TextBox2.Text + str + Chr(13) + Chr(10)
+        Next
+        
+        result = subStringString.Split(stringSeparators, StringSplitOptions.None)
+
+        For Each str As String In result
+
+            TextBox2.Text = TextBox2.Text + str + Chr(13) + Chr(10)
+        Next
+
+
+    End Sub
+
+    Private Sub tombbeolvas_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tombbeolvas.Click
+        Dim f As New IO.StreamReader("telkek.txt")
+        Dim i, j, k, l, m, n, szamlalo1, szamlalo2, szamlalo3, szamlalo4 As Integer
+        Dim Telekszam As Integer
+
+        Dim osszesadat As String
+        Dim result As String()
+
+        Dim spaceSeparator As Char() = New Char() {" "c}
+        Dim intSpacedString As String
+
+        Telekszam = CInt(f.ReadLine)
+        'Dim telek()
+        Dim word(Telekszam - 1)
+        Dim telek(Telekszam - 1) As String
+        Dim adatok((Telekszam - 1) * 5) As Integer
+
+        For i = 0 To Telekszam - 1
+            Dim sor As String = f.ReadLine
+            telek(i) = sor
+
+
+            tombbeolvasott.Text = tombbeolvasott.Text + telek(i) + Chr(13) + Chr(10)
+
+            osszesadat = Join(telek)
+            TextBox3.Text = osszesadat
+
+
+        Next
+
+        intSpacedString = osszesadat
+
+        result = intSpacedString.Split(spaceSeparator, StringSplitOptions.None)
+        k = 0
+        j = 0
+        For Each str As String In result
+
+            TextBox4.Text = TextBox4.Text + str + Chr(13) + Chr(10)
+
+
+            adatok(k) = CInt(str)
+            TextBox6.Text = TextBox6.Text + CStr(adatok(k)) + Chr(13) + Chr(10)
+            Console.WriteLine(adatok(k))
+            k = k + 1
+
+
+            
+
+        Next
+
+
+        'szétválogatás
+        Dim joletsor((Telekszam - 1) / 2, 2) As Integer 'páros számú telkek
+        Dim gazdagsor((Telekszam - 1) / 2, 2) As Integer 'páratlan számú telkek
+        m = 1
+        n = 2
+        l = 0
+        szamlalo1 = 0
+        szamlalo2 = 0
+
+
+        For l = 0 To ((Telekszam - 1) * 3 + 2) Step 3
+            TextBox7.Text = TextBox7.Text + CStr(adatok(l)) + Chr(13) + Chr(10)
+            If adatok(l) / 2 = Int(adatok(l) / 2) Then
+                joletsortext.Text = joletsortext.Text + " " + CStr(adatok(l)) + " " + CStr(adatok(l + 1)) + " " + CStr(adatok(l + 2)) + Chr(13) + Chr(10)
+                joletsor(szamlalo1, 0) = adatok(l) : joletsor(szamlalo1, 1) = adatok(l + 1) : joletsor(szamlalo1, 2) = adatok(l + 2)
+                joletsortext.Text = joletsortext.Text + " " + CStr(joletsor(szamlalo1, 0)) + " " + CStr(joletsor(szamlalo1, 1)) + " " + CStr(joletsor(szamlalo1, 2)) + Chr(13) + Chr(10)
+                szamlalo1 = szamlalo1 + 1
+                n = n + 2
+            Else
+                gazdagsortext.Text = gazdagsortext.Text + " " + CStr(adatok(l)) + " " + CStr(adatok(l + 1)) + " " + CStr(adatok(l + 2)) + Chr(13) + Chr(10)
+                gazdagsor(szamlalo2, 0) = adatok(l) : gazdagsor(szamlalo2, 1) = adatok(l + 1) : gazdagsor(szamlalo2, 2) = adatok(l + 2)
+                gazdagsortext.Text = gazdagsortext.Text + " " + CStr(gazdagsor(szamlalo2, 0)) + " " + CStr(gazdagsor(szamlalo2, 1)) + " " + CStr(gazdagsor(szamlalo2, 2)) + Chr(13) + Chr(10)
+                szamlalo2 = szamlalo2 + 1
+                m = m + 2
+            End If
+        Next
+
+        'rendezés házszám szerint
+
+        Dim i1, j1
+        Dim a, b, c, d, e1 As Integer
+        szamlalo3 = 0
+        szamlalo4 = 0
+
+        For i1 = 0 To 14 'n - 1
+            For j1 = i1 + 1 To 15 'n
+                If joletsor(i1, 0) > joletsor(j1, 0) Then
+                    a = joletsor(i1, 0) : b = joletsor(i1, 1) : c = joletsor(i1, 2)
+                    joletsor(i1, 0) = joletsor(j1, 0) : joletsor(i1, 1) = joletsor(j1, 1) : joletsor(i1, 2) = joletsor(j1, 2)
+                    joletsor(j1, 0) = a : joletsor(j1, 1) = b : joletsor(j1, 2) = c
+
+                End If
+            Next
+        Next
+
+        For szamlalo3 = 0 To 15
+            joletsorhsztext.Text = joletsorhsztext.Text + " " + CStr(joletsor(szamlalo3, 0)) + " " + CStr(joletsor(szamlalo3, 1)) + " " + CStr(joletsor(szamlalo3, 2)) + Chr(13) + Chr(10)
+
+        Next
+
+
+        For i1 = 0 To 14 'n - 1
+            For j1 = i1 + 1 To 15 'n
+                If gazdagsor(i1, 0) > gazdagsor(j1, 0) Then
+                    a = gazdagsor(i1, 0) : b = gazdagsor(i1, 1) : c = gazdagsor(i1, 2)
+                    gazdagsor(i1, 0) = gazdagsor(j1, 0) : gazdagsor(i1, 1) = gazdagsor(j1, 1) : gazdagsor(i1, 2) = gazdagsor(j1, 2)
+                    gazdagsor(j1, 0) = a : gazdagsor(j1, 1) = b : gazdagsor(j1, 2) = c
+
+                End If
+            Next
+        Next
+
+        For szamlalo4 = 0 To 15
+            gazdagsorhsztext.Text = gazdagsorhsztext.Text + " " + CStr(gazdagsor(szamlalo4, 0)) + " " + CStr(gazdagsor(szamlalo4, 1)) + " " + CStr(gazdagsor(szamlalo4, 2)) + Chr(13) + Chr(10)
+
+        Next
+
+        'számítások Jólétsor
+        Dim joletsorszamitott(15, 4) As Integer
+        Dim ado As Integer
+        For szamlalo3 = 0 To 15
+            joletsorszamitott(szamlalo3, 0) = joletsor(szamlalo3, 0) 'házszám
+            joletsorszamitott(szamlalo3, 1) = joletsor(szamlalo3, 1) ' szélesség
+            joletsorszamitott(szamlalo3, 2) = 70 - gazdagsor(szamlalo3, 2) 'telekhossz
+            joletsorszamitott(szamlalo3, 3) = (70 - gazdagsor(szamlalo3, 2)) * (joletsor(szamlalo3, 1)) 'terület
+
+
+            If joletsorszamitott(szamlalo3, 3) <= 700 Then
+                ado = joletsorszamitott(szamlalo3, 3) * 51
+            ElseIf joletsorszamitott(szamlalo3, 3) <= 1000 Then
+
+                ado = 700 * 51 + (joletsorszamitott(szamlalo3, 3) - 700) * 39
+            ElseIf joletsorszamitott(szamlalo3, 3) > 1000 Then
+                ado = 700 * 51 + 300 * 39 + 200
+            End If
+
+            If joletsorszamitott(szamlalo3, 1) <= 20 Then
+                ado = Int(8 / 10 * ado)
+            End If
+
+            ado = Int(ado / 100) * 100
+            joletsorszamitott(szamlalo3, 4) = ado 'adó
+
+
+
+        Next
+
+        For szamlalo3 = 0 To 15
+            joletsoreredmenyek.Text = joletsoreredmenyek.Text + " " + CStr(joletsorszamitott(szamlalo3, 0)) + " " + CStr(joletsorszamitott(szamlalo3, 1)) + " " + CStr(joletsorszamitott(szamlalo3, 2)) + " " + CStr(joletsorszamitott(szamlalo3, 3)) + " " + CStr(joletsorszamitott(szamlalo3, 4)) + Chr(13) + Chr(10)
+
+        Next
+
+        'kiírás fájlba
+        Dim g As New IO.StreamWriter("joletsor.csv")
+        For szamlalo3 = 0 To 15
+            g.WriteLine("{0};{1};{2};{3};{4}", joletsorszamitott(szamlalo3, 0), joletsorszamitott(szamlalo3, 1), joletsorszamitott(szamlalo3, 2), joletsorszamitott(szamlalo3, 3), joletsorszamitott(szamlalo3, 4))
+
+        Next
+        g.Close()
+
+
+        'számítások Gazdagsor
+        Dim gazdagsorszamitott(15, 4) As Integer
+        For szamlalo3 = 0 To 15
+            gazdagsorszamitott(szamlalo3, 0) = gazdagsor(szamlalo3, 0)
+            gazdagsorszamitott(szamlalo3, 1) = gazdagsor(szamlalo3, 1)
+            gazdagsorszamitott(szamlalo3, 2) = gazdagsor(szamlalo3, 2) 'telekhossz
+            gazdagsorszamitott(szamlalo3, 3) = (gazdagsor(szamlalo3, 1)) * (gazdagsor(szamlalo3, 2)) 'terület
+
+            gazdagsorszamitott(szamlalo3, 4) = 0 'adó
+
+
+
+        Next
+
+        For szamlalo3 = 0 To 15
+            gazdagsoreredmenyek.Text = gazdagsoreredmenyek.Text + " " + CStr(gazdagsorszamitott(szamlalo3, 0)) + " " + CStr(gazdagsorszamitott(szamlalo3, 1)) + " " + CStr(gazdagsorszamitott(szamlalo3, 2)) + " " + CStr(gazdagsorszamitott(szamlalo3, 3)) + " " + CStr(gazdagsorszamitott(szamlalo3, 4)) + Chr(13) + Chr(10)
+
+        Next
+
+
+        For i1 = 0 To 14 'n - 1
+            For j1 = i1 + 1 To 15 'n
+                If gazdagsorszamitott(i1, 3) > gazdagsorszamitott(j1, 3) Then
+                    a = gazdagsorszamitott(i1, 0) : b = gazdagsorszamitott(i1, 1) : c = gazdagsorszamitott(i1, 2) : d = gazdagsorszamitott(i1, 3) : e1 = gazdagsorszamitott(i1, 4)
+                    gazdagsorszamitott(i1, 0) = gazdagsorszamitott(j1, 0) : gazdagsorszamitott(i1, 1) = gazdagsorszamitott(j1, 1) : gazdagsorszamitott(i1, 2) = gazdagsorszamitott(j1, 2) : gazdagsorszamitott(i1, 3) = gazdagsorszamitott(j1, 3) : gazdagsorszamitott(i1, 4) = gazdagsorszamitott(j1, 4)
+                    gazdagsorszamitott(j1, 0) = a : gazdagsorszamitott(j1, 1) = b : gazdagsorszamitott(j1, 2) = c : gazdagsorszamitott(j1, 3) = d : gazdagsorszamitott(j1, 4) = e1
+
+                End If
+            Next
+        Next
+
+        For szamlalo3 = 0 To 15
+            gazdagsorteruletszerinttext.Text = gazdagsorteruletszerinttext.Text + " " + CStr(gazdagsorszamitott(szamlalo3, 0)) + " " + CStr(gazdagsorszamitott(szamlalo3, 1)) + " " + CStr(gazdagsorszamitott(szamlalo3, 2)) + " " + CStr(gazdagsorszamitott(szamlalo3, 3)) + " " + CStr(gazdagsorszamitott(szamlalo3, 4)) + Chr(13) + Chr(10)
+
+        Next
+
+    End Sub
+
+    Private Sub joletsortext_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles joletsortext.TextChanged
+
+    End Sub
+End Class
